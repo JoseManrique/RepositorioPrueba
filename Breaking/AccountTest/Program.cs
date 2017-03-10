e37f8808d8692d4b4d651a428de142a40a28f384
Clase <Clase>
2017-03-09 22:17:43 -0500

    Primer Programa

---
+++ Breaking/AccountTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using BankingModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Account myAccount;
+            //myAccount = new Account();
+
+            //Console.WriteLine("Nombre inicial de la cuenta:{0}", myAccount.Name);
+
+            //Console.WriteLine("Ingrese nombre:");
+            //string theName = Console.ReadLine();
+            //myAccount.Name = theName;
+
+            //Console.WriteLine("Nombre de la cuenta:{0}", myAccount.Name);
+
+        //    Account myAccount = new Account("Juan Perez");
+        //    Console.WriteLine("Nombre de la cuenta:{0}", myAccount.Name);
+
+
+            Account myAccount1 = new Account("Juan Perez", 50.0m);
+            Account myAccount2 = new Account("Jhon Doe", -80.0m);
+
+            Console.WriteLine("El saldo inicial de myAccount1({0}) es: {1} ", myAccount1.Name, myAccount1.Balance);
+            Console.WriteLine("El saldo inicial de myAccount2({0}) es: {1} ", myAccount2.Name, myAccount2.Balance);
+
+        }
+    }
+}
---
+++ Breaking/BankingModel/Account.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingModel
+{
+    public class Account
+    {
+        //private string _name;
+
+        //public string getName()
+        //{
+          //  return _name;
+        //}
+
+        //public void setName(string name)
+        //{
+          //  _name = name;
+        //}
+
+        //public string name
+        //{
+            //get
+          //  {
+             //   return _name;
+          //  }
+
+          //  set
+          //  {
+               // _name = value;
+           // }
+       // }
+
+        public string Name { get; set; }
+
+        private decimal balance;
+
+        public Account(string name, decimal initialBalance)
+        {
+                Name=name;
+                Balance = initialBalance;
+        }
+
+        public Account(string name)
+        {
+            Name = name;
+
+        }
+
+       
+
+        public decimal Balance
+        {
+            get 
+            { 
+                return balance; 
+            }
+
+            set 
+            {
+                if (value > 0.0m)
+                {
+                    balance = value;
+                }
+            }
+        }
+
+        public void Deposit(decimal depositAmount)
+        {
+            if(depositAmount > 0.0m)
+            {
+                Balance += depositAmount;
+            }
+        }
+
+
+
+    }
+}
